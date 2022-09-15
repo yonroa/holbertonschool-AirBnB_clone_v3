61cabaf344b60e225999b149ccfbed2249a8dfdd
yonroa <yonanderson11@gmail.com>
2022-09-15 15:46:31 -0500

    Upload task 3 with errors

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,25 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID,
+        or None if not found"""
+        try:
+            return str(self.__session.query(cls).filter(cls.id == id).first())
+        except:
+            return None
+    
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage.
+        """
+        counter = 0
+        if cls in classes:
+            objs = self.__session.query(classes[cls]).all()
+            return len(objs)
+        else:
+            for clase in classes:
+                objs = self.__session.query(classes[clase]).all()
+                counter += len(objs)
+        return counter
