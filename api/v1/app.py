8600fc7651dca178d6fdf046d094e91c2c49f4a2
yonroa <yonanderson11@gmail.com>
2022-09-16 18:57:35 -0500

    Upload api before state

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,18 +4,20 @@
 from models import storage
 from api.v1.views import app_views
 import flask
-from flask import appcontext_tearing_down
 import os
 
 app = flask.Flask(__name__)
 
 
-app.register_blueprint(app_views)
-
+app.register_blueprint(app_views, url_prefix='/api/v1')
 @app.teardown_appcontext
-def app_api():
+def recover_api(self):
     """Method app for api"""
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return flask.make_response(flask.jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """Index file"""
 
-import api.v1.views
+from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@api.v1.views.app_views.route("/status")
+classes = {"Amenity": 'Amenity', "BaseModel": 'BaseModel', "City": 'City',
+           "Place": 'Place', "Review": 'Review', "State": 'State', "User": 'User'}
+
+
+@app_views.route("/status")
 def api_return():
     """Method that returns the status of the json file"""
-    return jsonify({"status": "OK"})
+    return jsonify({'status': 'OK'})
+
+@app_views.route("/stats")
+def objects_count():
+    """retrieves the number of each objects by type"""
+    new_dict = {}
+    for k, v in classes.items():
+        new_dict[k] = storage.count(v)
+    return new_dict
