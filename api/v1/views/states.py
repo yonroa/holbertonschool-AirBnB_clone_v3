ee88a64d19cba8ec4c3d44a8c7eb65d5bbed369a
dianabanguera <dianabangueravillalobo@gmail.com>
2022-09-17 14:20:56 -0500

    7. State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Update states"""
 
+from crypt import methods
 from os import abort
+from unicodedata import name
+from urllib import request
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -24,3 +27,47 @@ def one_state(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
+
+@app_views.route("/states/<string:state_id>", methods=["DELETE"])
+def state_delete(state_id):
+    """Method that deletes a state object"""
+    state = storage.get("State", state_id)
+    if not state:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify(({}), 200)
+
+
+@app_views.route('/states', methods=['POST'])
+def state_post():
+    """Method that creates a state"""
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+
+    if name not in data:
+        abort(400, description="Missing name")
+
+    instance = State(**data)
+    instance.save()
+    return jsonify(instance.to_dict(), 201)
+
+
+@app_views.route("/states/<string:state_id>", methods=['PUT'])
+def state_put(state_id):
+    """Method that puts a state"""
+    state = storage.get("State", state_id)
+    data = request.get_json()
+    if not state:
+        abort(404)
+    if not data:
+        abort(400, description="Not a JSON")
+
+    ignore = ['id', 'created_at', 'updated_at']
+
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(state, key, value)
+        storage.save()
+        return jsonify(state.todict(), 200)
