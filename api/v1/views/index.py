1b32d9dc43cf4749bd4a720b288e9b7ecac1f5f9
yonroa <yonanderson11@gmail.com>
2022-09-17 18:13:55 -0500

    Fix index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,11 +4,18 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-classes = {"Amenity": 'Amenity', "BaseModel": 'BaseModel',
-           "City": 'City', "Place": 'Place', "Review": 'Review',
-           "State": 'State', "User": 'User'}
+classes = {"Amenity": Amenity, "BaseModel": BaseModel,
+           "City": City, "Place": Place, "Review": Review,
+           "State": State, "User": User}
 
 
 @app_views.route("/status")
